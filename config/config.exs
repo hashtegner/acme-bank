e0090f69fecd9d8fbee9ac5d2ed4054c76b3ddaf
Alessandro Tegner <alessandro.tegner@gmail.com>
2019-11-10 08:33:43 -0300

    Configure binary id as primary key for migrations

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,6 @@
 import Config
 
 config :acme_bank, ecto_repos: [AcmeBank.Repo]
+config :acme_bank, AcmeBank.Repo, migration_primary_key: [name: :id, type: :binary_id]
 
 import_config "#{Mix.env()}.exs"
