0a84982ab777c3c6944e2793a2f17e98719990ac
Alessandro Tegner <alessandro.tegner@gmail.com>
2019-11-11 13:36:06 -0300

    Setup service mocks

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,8 @@
 import Config
 
+config :acme_bank, :accounts_service, AcmeBank.Accounts
+config :acme_bank, :wallet_service, AcmeBank.Wallet
+
 config :acme_bank, ecto_repos: [AcmeBank.Repo]
 config :acme_bank, AcmeBank.Repo, migration_primary_key: [name: :id, type: :binary_id]
 
--- config/test.exs
+++ config/test.exs
@@ -1,5 +1,8 @@
 import Config
 
+config :acme_bank, :accounts_service, AcmeBank.AccountsMock
+config :acme_bank, :wallet_service, AcmeBank.WalletMock
+
 config :acme_bank, AcmeBank.Repo,
   database: "acme_bank_repo_test",
   username: "postgres",
